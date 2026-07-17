0b76e9dc887016170a7b2e220648f5e768d80742
Alexander-186 <alexfordjoyce@gmail.com>
2026-07-17 00:58:16 -0400

    Add interactive features and data handling for predictions
    Enhance the FIFA World Cup Match Predictor by adding interactive features, ensuring data integrity, and providing visualization for predicted outcomes.

--- app.py
+++ app.py
@@ -9,6 +9,9 @@ model_columns = joblib.load("model_columns.pkl")
 # Load dataset containing international matches from 1872 - 2026. 
 df = pd.read_csv("results.csv")
 
+# Adding a title and feature for the tournaments. So instead of producing every single tournament for each international match,
+my model can only focus on the FIFA World Cup/FIFA World Cup qualification matches. 
+
 st.title("⚽ FIFA World CupMatch Predictor ⚽")
 
 WC_tournaments = [
@@ -18,11 +21,18 @@ WC_tournaments = [
 
 df = df[df["tournament"].isin(WC_tournaments)]
 
+# Making sure teams dont have null values and producing them as unique strings so that they are easily understandable. 
+
 home_teams = df["home_team"].dropna().astype(str).unique()
 away_teams = df["away_team"].dropna().astype(str).unique()
 
+# Sorts the tournaments in alphabetical order, avoiding duplicates and storing the list of tournmanents into one of the
+features. 
+
 tournaments = sorted(df["tournament"].unique()) 
 
+# Streamlit code so that website is interactive and users can directly enter what team and tournament they are selecting. 
+# It also gives them the option of choosing whether the game is on equal ground. 
 
 home_team = st.selectbox(
     "Select Home Team",
@@ -41,6 +51,9 @@ tournament = st.selectbox(
 
 neutral = st.checkbox("Neutral Venue")
 
+# When the "predict" button is pressed, a dataframe is accessed regarding the home team, away team, tournament, and neutral.
+# get_dummies is used for each of the columns so that it operates under the LogisticRegression model. 
+
 if st.button("Predict"):
 
     new_match = pd.DataFrame({
@@ -55,12 +68,15 @@ if st.button("Predict"):
         columns=["home_team", "away_team", "tournament"]
     )
 
-    # Make sure columns match the training data
+    # Making sure columns match the training data being used. Then proceeds to make a prediction and used probability for the match.
+    
     new_match = new_match.reindex(columns=model_columns, fill_value=0)
 
     prediction = model.predict(new_match)[0]
 
     probabilities = model.predict_proba(new_match)[0]
+
+    # If successful, the predicted outcome will be generated with a probability for each team's victory or draw. 
 
     st.success(f"Predicted Outcome: {prediction}")
 
@@ -70,6 +86,8 @@ if st.button("Predict"):
     })
 
     st.dataframe(results)
+
+    # An extra feature with a bar chart for the DataFrame that gives a bit of visualization to the user. 
 
     chart_data = pd.DataFrame({
         "Probability": probabilities * 100
